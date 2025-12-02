5a42bf58aa7ab2b7a1c3ae703f6a6c758be172cc
Nadia Kolodiiuk <nkolodeyuk@gmail.com>
2025-12-02 07:25:26 +0200

    add files for deployment

---
+++ CompileTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace PlugPlay.Infrastructure;
+
+public class CompileTimeDbContextFactory : IDesignTimeDbContextFactory<PlugPlayDbContext>
+{
+    public PlugPlayDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<PlugPlayDbContext>();
+        optionsBuilder.UseNpgsql(
+            "Host=plugplay-db-1;Port=5432;Database=plugplay;Username=myuser;Password=mypassword");
+
+        return new PlugPlayDbContext(optionsBuilder.Options);
+    }
+}
---
+++ baseApi.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import {storage} from "../utils/StorageService.ts";
+
+export const API_BASE_URL = 'https://plugplay.dev1.dev'; // port may be different
+
+export const baseApi = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${API_BASE_URL}/api`,
+    prepareHeaders: (headers) => {
+      const token = storage.getAccessToken();
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`);
+      }
+
+      if (!headers.has('Content-Type')) {
+        headers.set('Content-Type', 'application/json');
+      }
+      return headers;
+    },
+  }),
+  endpoints: () => ({}),
+});
