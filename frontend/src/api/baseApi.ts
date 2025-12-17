ea2fdad43fb4921cb01682de9f7218c02ce230ea
Dzinious <99750838+Dzinious@users.noreply.github.com>
2025-12-17 13:25:47 +0200

    add conditionional endpoint content type in baseApi

--- frontend/src/api/baseApi.ts
+++ frontend/src/api/baseApi.ts
@@ -7,15 +7,19 @@ export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: `${API_BASE_URL}/api`,
-    prepareHeaders: (headers) => {
+    prepareHeaders: (headers, api) => {
       const token = storage.getAccessToken();
       if (token) {
         headers.set('Authorization', `Bearer ${token}`);
       }
 
-      // if (!headers.has('Content-Type')) {
-      //   headers.set('Content-Type', 'application/json');
-      // }
+      const skipJsonContentType = [
+        "uploadProductImage",  
+      ];
+
+      if (!skipJsonContentType.includes(api.endpoint)) {
+        headers.set("Content-Type", "application/json");
+      }
 
       return headers;
     },
